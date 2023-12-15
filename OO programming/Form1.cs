75504f4c548f7ae7ceff7474eab87d4b552bc49d
alex4 <alex4@ALEX>
2023-12-15 19:15:11 +1100

    Add project files.

---
+++ OO programming/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using System.IO;
+using System.Globalization;
+using CsvHelper;
+
+
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using System.Net.Configuration;
+using System.Net.NetworkInformation;
+using ServiceStack;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+
+
+
+
+namespace OO_programming
+{
+    public partial class Form1 : Form
+    {
+
+
+
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Add code below to complete the implementation to populate the listBox
+            // by reading the employee.csv file into a List of PaySlip objects, then binding this to the ListBox.
+            // CSV file format: <employee ID>, <first name>, <last name>, <hourly rate>,<taxthreshold>
+
+
+
+
+
+
+            List<PaySlip> EmployeeDetailList = new List<PaySlip>();
+            List<PayCalculator> payCalculators = new List<PayCalculator>();
+            List<PayCalculatorNoThreshold> payCalculatorNoThresholds = new List<PayCalculatorNoThreshold>();
+            List<PayCalculatorWithThreshold> payCalculatorWithThresholds = new List<PayCalculatorWithThreshold>();
+
+
+
+
+
+
+
+            TextBox1_Load();
+
+
+
+
+
+        }
+
+
+
+
+
+        private void TextBox1_Load()
+        {
+
+            string Path = @"C:\Users\alex4\Downloads\employee.csv";
+            StreamReader reader = null;
+
+            string employeeID = "Employee ID";
+            string firstname = "First Name";
+            string lastname = "Last Name";
+            string hourlyrate = "Hourly Rate";
+            string taxthreshold = "Taxthreshold";
+
+            PaySlip EmployeeDetailList = new PaySlip();
+            EmployeeDetailList.EmployeeID = employeeID;
+            EmployeeDetailList.FirstName = firstname;
+            EmployeeDetailList.LastName = lastname;
+            EmployeeDetailList.HourlyRate = hourlyrate;
+            EmployeeDetailList.TaxThreshold = taxthreshold;
+
+
+
+            if (File.Exists(Path))
+            {
+                reader = new StreamReader(Path);
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var Values = line.Split(',');
+                    listBox1.Items.Add(employeeID);
+                    listBox1.Items.Add(Values[0]);
+                    listBox1.Items.Add(firstname);
+                    listBox1.Items.Add(Values[1]);
+                    listBox1.Items.Add(lastname);
+                    listBox1.Items.Add(Values[2]);
+                    listBox1.Items.Add(hourlyrate);
+                    listBox1.Items.Add(Values[3]);
+                    listBox1.Items.Add(taxthreshold);
+                    listBox1.Items.Add(Values[4]);
+
+
+
+
+
+                }
+
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Add code below to complete the implementation to populate the
+            // payment summary (textBox2) using the PaySlip and PayCalculatorNoThreshold
+            // and classes object and methods.
+
+
+            PaySlip EmployeeDetailList = new PaySlip();
+            EmployeeDetailList.EmployeeID = "Employee ID";
+            PayCalculatorNoThreshold payCalculatorNoThresholds = new PayCalculatorNoThreshold();
+
+
+
+          
+            string Path1 = @"C:\Users\alex4\Downloads\taxrate-nothreshold.csv";
+            StreamReader reader1 = null;
+
+          
+
+
+
+
+
+
+            if (File.Exists(Path1))
+            {
+                reader1 = new StreamReader(Path1);
+
+                while (!reader1.EndOfStream)
+                {
+                    var line1 = reader1.ReadLine();
+                    var Values1 = line1.Split(',');
+
+                    listBox1.Items.Add(Values1[0]);
+
+
+
+                }
+
+
+            }
+        }
+
+
+
+                private void button2_Click(object sender, EventArgs e)
+                {
+                    // Add code below to complete the implementation for saving the
+                    // calculated payment data into a csv file.
+
+                    // File naming convention: Pay_<full name>_<datetimenow>.csv
+                    // Data fields expected - EmployeeId, Full Name, Hours Worked, Hourly Rate, Tax Threshold, Gross Pay, Tax, Net Pay, Superannuation
+
+
+
+
+                }
+
+
+            }
+        }
+
+    
+
+
+
+
+
---
+++ OO programming/Program.cs
@@ -0,0 +1,176 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Windows.Forms;
+using CsvHelper;
+using System.IO;
+using static System.Windows.Forms.LinkLabel;
+using System.Security.Policy;
+using OO_programming;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace OO_programming
+{
+
+    static class Program
+    {
+
+
+
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+
+
+
+
+
+
+
+                }
+            }
+
+    /// <summary>
+    /// Class a capture details accociated with an employee's pay slip record
+    /// </summary>
+
+
+
+
+    public class PaySlip
+    {
+
+
+        public string EmployeeID { get; set; }
+
+
+        public string FirstName { get; set; }
+
+
+        public string LastName { get; set; }
+
+
+        public string HourlyRate { get; set; }
+
+        public string TaxThreshold { get; set; }
+
+        public int HoursWorked { get; set; }
+
+        public string GrossPay { get; set; }
+
+        public string Tax { get; set; }
+        public string NetPay { get; set; }
+
+        public string SupperAnnuation { get; set; }
+
+
+
+
+
+      
+
+       
+
+         
+
+
+
+                }
+
+
+
+
+
+            }
+
+
+        
+    
+        
+
+                 
+
+
+
+
+
+
+
+
+
+/// <summary>
+/// Base class to hold all Pay calculation functions
+/// Default class behaviour is tax calculated with tax threshold applied
+/// </summary>
+public class PayCalculator
+        {
+
+            public string HourlyRate { get; set; }
+
+            public string TaxThreshold { get; set; }
+
+            public int HoursWorked { get; set; }
+
+            public string GrossPay { get; set; }
+
+            public string Tax { get; set; }
+            public string NetPay { get; set; }
+
+            public string SupperAnnuation { get; set; }
+
+
+        }
+
+
+
+
+
+        /// <summary>
+        /// Extends PayCalculator class handling No tax threshold
+        /// </summary>
+        public class PayCalculatorNoThreshold : PayCalculator
+        {
+
+        
+
+            public PayCalculatorNoThreshold()
+            {
+               
+                
+
+            }
+
+
+
+
+        }
+
+        /// <summary>
+        /// Extends PayCalculator class handling With tax threshold
+        /// </summary>
+        public class PayCalculatorWithThreshold : PayCalculator
+        {
+
+            public PayCalculatorWithThreshold()
+            {
+
+
+            }
+
+
+        }
+
+    
+
+
+    
